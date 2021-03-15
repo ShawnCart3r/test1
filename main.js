650e27b8d56be93c924d7817cea34e06da1575e1
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2021-03-15 18:27:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,6 @@
 //navtoggle
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-});
-
-//navbar sticky
-window.onscroll = function() {myFunction()};
-
-
-let navbar = document.getElementById("navbar");
-
-
-let sticky = navbar.offsetTop;
-
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
 
 //greeting
  
